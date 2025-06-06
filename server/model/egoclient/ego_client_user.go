bf02ef2f5b09c4926075544c59db3a653fc0967a
邹嘉旭 <zoujiaxv7891@gmail.com>
2025-06-07 04:47:05 +0800

    6.7_2

--- server/model/egoclient/ego_client_user.go
+++ server/model/egoclient/ego_client_user.go
@@ -15,11 +15,11 @@ type EgoClientUser struct {
 	UUID        uuid.UUID `json:"uuid" form:"uuid" gorm:"column:uuid;" `                                //用户ID
 	UserID      *string   `json:"userID" form:"userID" gorm:"column:user_id;" binding:"required"`       //用户ID
 	Password    *string   `json:"password" form:"password" gorm:"column:password;" binding:"required"`  //密码
-	Username    *string   `json:"username" form:"username" gorm:"default:新用户;column:username;"`      //用户名
+	Username    *string   `json:"username" form:"username" gorm:"default:新用户;column:username;"`         //用户名
 	Avatar      string    `json:"avatar" form:"avatar" gorm:"column:avatar;"`                           //头像
 	Gender      *string   `json:"gender" form:"gender" gorm:"column:gender;"`                           //性别
 	Description *string   `json:"description" form:"description" gorm:"column:description;"type:text;"` //用户简介
-	AuthorityId uint      `json:"authorityId" gorm:"default:999;comment:用户角色ID"`                    // 用户角色ID
+	AuthorityId uint      `json:"authorityId" gorm:"default:999;comment:用户角色ID"`                        // 用户角色ID
 }
 
 func (s *EgoClientUser) GetUsername() string {
