d42b469c3f1d06ea6ab07058f60bdfbcc48b537a
邹嘉旭 <zoujiaxv7891@gmail.com>
2025-06-06 23:16:29 +0800

    6.6_2

--- server/initialize/gorm_biz.go
+++ server/initialize/gorm_biz.go
@@ -2,11 +2,12 @@ package initialize
 
 import (
 	"github.com/flipped-aurora/gin-vue-admin/server/global"
+	"github.com/flipped-aurora/gin-vue-admin/server/model/egoclient"
 )
 
 func bizModel() error {
 	db := global.GVA_DB
-	err := db.AutoMigrate()
+	err := db.AutoMigrate(egoclient.EgoClientUser{})
 	if err != nil {
 		return err
 	}
